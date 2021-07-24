1b89e5ae49afe786451112dac391fdbcd5550847
Максим Шамшурин <shamshurin.mak@yandex.ru>
2021-07-24 21:34:35 +0300

    Поправил оформление

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -5,4 +5,6 @@ class IsOwnerOrReadOnly(BasePermission):
     """Проверяет наличие прав на изменение/удаление объекта"""
 
     def has_object_permission(self, request, view, obj):
-        return (request.method == 'GET') or (obj.author == request.user)
+        return (
+            (request.method in ('GET', 'POST')) or (obj.author == request.user)
+        )
